347d16595e613e9942b170842516133a02d69ba7
Devis <22038252+DevisWeb@users.noreply.github.com>
2020-12-10 14:35:13 +0100

    Delete specific task-card from task-container

--- public/App/index.js
+++ public/App/index.js
@@ -10,8 +10,23 @@ const doneList = document.querySelector(".task-container-done");
 // *** EVENT LISTENERS **************
 // --> listen on what user clicks --> execute according function
 todoButton.addEventListener("click", addTodoCard); // execute funtion addToDoCard
+// execute function deleteAndCheck
+// --> listen to whole task-container to add funcionality based on what is clicked
+todoList.addEventListener("click", deleteAndCheck);
 
 // *** FUNCTIONS ********************
+function deleteAndCheck(e) {
+  // console.log(e.target); // test if listens on differnt clicks to whole container
+  // --> grab specific item we are clicking on:
+  const clickItem = e.target;
+  // DELETE DIV.TASK-CARD (= targeted task clicked to be deleted)
+  if (clickItem.classList[0] === "task-delete") {
+    // remove whole div.task-card = highest parent of item clicked
+    const taskChoosen = clickItem.parentElement.parentElement.parentElement;
+    taskChoosen.remove();
+  }
+}
+
 function addTodoCard(event) {
   event.preventDefault(); // prevent form from submitting/reload
 
@@ -19,12 +34,14 @@ function addTodoCard(event) {
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("task-card"); // add class to div
   todoDiv.classList.add("task-todo"); // -- " --
+
   // --- < 1st inner div of div.task-card:
   // = < < CREATE inner div.task-content+vertical that is inside div.task-card
   const newTodoContent = document.createElement("div");
   newTodoContent.classList.add("task-content");
   newTodoContent.classList.add("vertical");
   todoDiv.appendChild(newTodoContent); // grab parent todoDiv -> append child newTodoContent
+
   // ---- < inner-elements of div.task-content:
   // = < < < CREATE i-inner element h2.task-title
   const newTodoTitle = document.createElement("h2");
@@ -47,22 +64,23 @@ function addTodoCard(event) {
   newTodoControls.classList.add("task-controls");
   newTodoControls.classList.add("task-controls-todo");
   newTodoContent.appendChild(newTodoControls);
+
   // ------- < CREATE 3 inner elements of task-controls:
   // = < < < < CREATE button a.task-DELETE
   const trashBtn = document.createElement("a");
-  trashBtn.href = "#";
+  trashBtn.href = "#delete";
   trashBtn.classList.add("task-delete");
   trashBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
   newTodoControls.appendChild(trashBtn); // append to Controls div
   // = < < < < CREATE button a.task-EDIT
   const editBtn = document.createElement("a");
-  editBtn.href = "#";
+  editBtn.href = "#edit";
   editBtn.classList.add("task-edit");
   editBtn.innerHTML = '<i class="fas fa-edit"></i>';
   newTodoControls.appendChild(editBtn);
   // = < < < < CREATE button a.task-COMPLETE
   const completeBtn = document.createElement("a");
-  completeBtn.href = "#";
+  completeBtn.href = "#complete";
   completeBtn.classList.add("task-complete");
   completeBtn.innerHTML = '<i class="fas fa-check"></i>';
   newTodoControls.appendChild(completeBtn);

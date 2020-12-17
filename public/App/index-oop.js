a2a84c3fe54c9e015962290659275dd7309ed712
Devis <22038252+DevisWeb@users.noreply.github.com>
2020-12-17 13:42:21 +0100

    Migrate deleteCheckEdit | Fix console error-message
    * Remove static from TaskList
      --> instantiate TaskList Object in Event Handlers
    * Migrate deleteCheckEdit into index-oop.js
    * Add correspondong methods to TaskList
    
    * Fix console error-message
      --> Remove left-overs from index.html,
          that were still causing error-messages in the console

--- public/App/index-oop.js
+++ public/App/index-oop.js
@@ -2,7 +2,11 @@
 import Task from "./models/Task.js";
 import TaskList from "./models/TaskList.js";
 
-// ### Event-Handler: Add a new Task
+const todoList = document.querySelector(".task-container");
+const doneList = document.querySelector(".task-container-done");
+
+// ################################################################################################
+// ### Event-Handler: ADD a new Task
 document
   .querySelector(".button-add-task")
   .addEventListener("click", addTodoCard); // execute funtion addToDoCard
@@ -15,24 +19,81 @@ function addTodoCard(event) {
     description = document.querySelector("#form-task-description").value,
     dueDate = document.querySelector("#form-task-duedate").value;
 
-  // Instantiate a new task
+  // INSTANTIATE a new task
   const task = new Task(title, description, dueDate);
-  console.log(task); // console.log(task);
+  // console.log(task);
+
+  // INSTANTIATE a TaskList object (takes care of adding task to the list containers)
+  const taskList = new TaskList();
+  console.log("see prototype taskList: ");
+  console.log(taskList);
 
   // prevent form from submitting when fields are empty:
   if (title === "" || description === "" || dueDate === "") {
     const message = "Please add information to all fields.";
-    TaskList.displayCustomAlert(message, "alert-empty-fields"); //pass message and class of alertType based on event
+    taskList.displayCustomAlert(message, "alert-empty-fields"); //pass message and class of alertType based on event
+    //taskList.displayCustomAlert(message, "alert-empty-fields"); //pass message and class of alertType based on event
   } else {
-    const message = "New task added to list.";
-    TaskList.displayCustomAlert(message, "alert-success");
-
     // Add new task to be displayed on todoList
-    // with TaskList method addTaskToList, that creates a card calling 'createTodoCard':
-    TaskList.addTaskToList(task);
+    // with TaskList method 'addTaskToList', that creates a card calling 'createTodoCard':
+    taskList.addTaskToList(task);
 
     // CLEAR Form Input.Values
-    TaskList.clearForm();
+    taskList.clearForm();
+
+    // ALERT: if task successfully added --> display alert (uses styles from class '.alert-success', see alerts.css)
+    const message = "New task added to list.";
+    taskList.displayCustomAlert(message, "alert-success");
+  }
+}
+// ################################################################################################
+
+// LISTEN to both task-container with class .task-container --> add funcionality (method) based on what is clicked
+// --> listen to whole task-container --> add funcionality based on what is clicked
+todoList.addEventListener("click", deleteCheckEdit); // execute funtion deleteCheckEdit + further function
+doneList.addEventListener("click", deleteCheckEdit); // important for actions on click in second container for done tasks!
+// note: this is another way of structuring the eventlistener with its handlers
+function deleteCheckEdit(e) {
+  // Instantiate TaskList
+  const task = new Task();
+  const taskList = new TaskList();
+  // ### Event-Handler: DELETE a task that is displayed on a .task-container
+  // DELETE DIV.TASK-CARD (= targeted task clicked to be deleted):
+  const taskSelected = e.target.parentElement.parentElement.parentElement;
+
+  // DELETE with method deleteTask of Class TaskList
+  if (e.target.classList.contains("task-delete")) {
+    taskList.deleteTask(e.target, taskSelected); // same as in index.js 'taskChoosen'
+
+    const message = "Task has been removed from list.";
+    taskList.displayCustomAlert(message, "alert-success");
+    e.preventDefault();
+  }
+
+  // CHECK MARK TASK:
+  if (
+    e.target.classList.contains("task-complete") ||
+    e.target.classList.contains("task-uncomplete")
+  ) {
+    taskList.moveTaskCard(e.target, todoList, doneList, taskSelected); // moves card to other container (todoList or doneList)
+    task.markAsDone();
+    taskList.toggleStyleTaskCard(e.target, taskSelected); // toggle classes to change styles and icon
+    taskList.hideShowElement(taskSelected); // removes class .enableEditing to hide input-fields
+  }
+
+  // --> EDIT DIV.TASK-CARD content
+  if (e.target.classList.contains("task-edit")) {
+    console.log("show class list of class selected before toggle:");
+    console.log(taskSelected.classList); // class status before
+
+    // toggle (.enableEditing) between read and edititing whole card:
+    // --> this class controls display(block/none) of fields in task_cards.css
+    taskSelected.classList.toggle("enableEditing");
+
+    console.log("show class list of class selected after toggle:");
+    console.log(taskSelected.classList); // class status after
+
+    taskList.editTaskCard(taskSelected);
   }
 }
 
--- public/App/models/TaskList.js
+++ public/App/models/TaskList.js
@@ -4,14 +4,15 @@
 export default class TaskList {
   // TAKE the whole DIV.TASK-CARD created in function 'createTodoCard' and
   // APPEND (paste) it to the actual div.task-container (see index.html) for 'todoList' or 'doneList':
-  static addTaskToList(task) {
+  addTaskToList(task) {
     const todoList = document.querySelector(".task-container"); // grab todoList Container from the DOM
     todoList.appendChild(
       createTodoCard(task._title, task._description, task._dueDate)
     ); // todoList.appendChild(todoDiv);
   }
+
   // Method - Display alert to prevent from submitting empty form
-  static displayCustomAlert(message, alertType) {
+  displayCustomAlert(message, alertType) {
     // construct alert element, create div
     const div = document.createElement("div");
     // Add class to style alert
@@ -31,11 +32,93 @@ export default class TaskList {
     }, 2000);
   }
 
+  // Method - Delete TaskCard
+  deleteTask(target, taskSelected) {
+    if (target.className === "task-delete") {
+      taskSelected.remove();
+    }
+  }
+
   // Method - Clear Form
-  static clearForm() {
+  clearForm() {
     // note: passing as variables did not work --> select items directly works:
     document.querySelector("#form-task-title").value = "";
     document.querySelector("#form-task-description").value = "";
     document.querySelector("#form-task-duedate").value = "";
+  }
+
+  // Method - Move TaskCard
+  moveTaskCard(target, todoList, doneList, taskSelected) {
+    if (taskSelected.parentElement.classList.contains("task-container-done")) {
+      alert(
+        "Are you sure we're still not done?\nSo we' ll code the whole damn night again, won't we?\nShould think about creating a SleepDeptCalculator... \n;-)"
+      );
+      todoList.appendChild(target.parentElement.parentElement.parentElement);
+    } else {
+      alert(
+        "YES! We did it. Now ready for eatin a big family-pizza with extra of everything and grab some beer!"
+      );
+      doneList.appendChild(target.parentElement.parentElement.parentElement);
+    }
+  }
+
+  // Method - Change Style of Card
+  toggleStyleTaskCard(target, taskSelected) {
+    target.classList.toggle("task-uncomplete");
+    taskSelected.classList.toggle("task-done");
+    target.firstChild.classList.toggle("fa-times");
+    target.parentElement.classList.toggle("task-controls-done");
+  }
+
+  // Method - Hide(if done) or Show (undone+edit mode) Input fields
+  hideShowElement(taskSelected) {
+    console.log("function hideShowElement called:");
+    if (taskSelected.classList.contains("task-done")) {
+      console.log(" --> SHOW Input fields");
+      taskSelected.classList.remove("enableEditing");
+    }
+  }
+
+  // Method - Hide
+  editTaskCard(taskSelected) {
+    console.log(
+      "clicked edit icon\n --> executes function editToDoCard\n --> enable editing or 'save' text changes"
+    );
+    // define input-fields:
+    const inputEnabledTitle = taskSelected.querySelector("#enableInputTitle");
+    const inputEnabledDesc = taskSelected.querySelector("#enableInputDesc");
+    const inputEnabledDue = taskSelected.querySelector("#enableInputDue");
+    // define labels to pass label-text to input-fields:
+    const labelTodoTitle = taskSelected.querySelector(".task-title");
+    const labelTodoDesc = taskSelected.querySelector(".task-description");
+    const labelTodoDue = taskSelected.querySelector(".task-duedate");
+
+    console.log("Show Class List inMethod Edit");
+    console.log(taskSelected.classList);
+    if (taskSelected.classList.contains("enableEditing")) {
+      // show input field that now contains the former label text for editing
+      console.log(
+        " Input-fields get text from labels\n --> text can now be edited"
+      );
+      inputEnabledTitle.value = labelTodoTitle.innerText;
+      inputEnabledDesc.value = labelTodoDesc.innerText;
+      inputEnabledDue.value = labelTodoDue.innerText;
+    } else {
+      console.log(
+        " 1) if not empty, labels get text from Input-fields;\n 2) input fields should hide"
+      );
+      //if (taskSelected.querySelector("#enableInputTitle").value === "") {
+      if (
+        inputEnabledTitle.value === "" ||
+        inputEnabledDesc.value === "" ||
+        inputEnabledDue.value === ""
+      ) {
+        alert("No empty fields please.");
+        return; // sb-todo: use method for custom alert
+      }
+      labelTodoTitle.innerText = inputEnabledTitle.value;
+      labelTodoDesc.innerText = inputEnabledDesc.value;
+      labelTodoDue.innerText = inputEnabledDue.value;
+    }
   }
 }
